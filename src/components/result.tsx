c204f2669517f8e4baec28079d3e013c7577c9be
Diego Ramirez <gustavormzrmz@gmail.com>
2022-03-29 13:40:07 -0600

    implemented context

--- src/components/result.tsx
+++ src/components/result.tsx
@@ -1,8 +1,14 @@
-import React from 'react';
+import React, { useContext } from 'react';
+
+import { Store as FibonacciStore} from '../context/store/fibonacci';
 
 const Result = () => {
+  const { state } = useContext(FibonacciStore);
   return (
-    <h3>Fibonacci Result: </h3>
+    <>
+      <h3>Fibonacci Result: </h3>
+      <p>{ state.result }</p>
+    </>
   );
 };
 
