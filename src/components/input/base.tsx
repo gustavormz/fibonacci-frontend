7ca9a8f33cff9aad78891aab3a30b7be2bace1f0
Diego Ramirez <gustavormzrmz@gmail.com>
2022-03-29 13:39:59 -0600

    implemented context

--- src/components/input/base.tsx
+++ src/components/input/base.tsx
@@ -1,4 +1,13 @@
-import React, { FC, InputHTMLAttributes } from 'react';
+import React, {
+  FC,
+  InputHTMLAttributes,
+  useContext,
+  ChangeEvent
+} from 'react';
+
+import { Store as FibonacciStore} from '../../context/store/fibonacci';
+import { Types } from '../../context/reducer/fibonacci';
+import Api from '../../api';
 
 interface IInputProps extends InputHTMLAttributes<HTMLInputElement> {
   name: string;
@@ -7,11 +16,49 @@ interface IInputProps extends InputHTMLAttributes<HTMLInputElement> {
 const InputBase: FC<IInputProps> = ({
   name,
   ...restProps
-}) => (
-  <div className='input-base-container'>
-    <label htmlFor='input-n-term'>{ name }</label>
-    <input className='input-base' type={'number'} id='input-n-term' { ...restProps } />
-  </div>
-);
+}) => {
+  const { state, dispatch } = useContext(FibonacciStore);
+
+  const handleOnChange = async (event: ChangeEvent<HTMLInputElement>) => {
+    event.preventDefault();
+    let nTerm = 0;
+    try {
+      nTerm = parseInt(event.target.value, 10);
+    } catch (error) {
+      nTerm = 0;
+    }
+
+    dispatch({
+      type: Types.SET_N_TERM,
+      payload: { nTerm }
+    });
+
+    const response = await Api().getNTermFibonacci({ nTerm });
+    const result = response.error ?
+      response.message:
+      response.data.nTerm;
+    dispatch({
+      type: Types.SET_RESULT,
+      payload: {
+        result
+      }
+    });
+  };
+
+  return (
+    <div className='input-base-container'>
+      <label htmlFor='input-n-term'>{ name }</label>
+      <input
+        min={ 0 }
+        max={ 78 }
+        value={ state.nTerm }
+        onChange={ handleOnChange }
+        className='input-base'
+        type={ 'number' }
+        id='input-n-term'
+        { ...restProps } />
+    </div>
+  )
+};
 
 export default InputBase;
