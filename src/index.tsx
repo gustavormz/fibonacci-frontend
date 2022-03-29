f99da53e05d574b06e19b98dee7783d72d9cdf61
Diego Ramirez <gustavormzrmz@gmail.com>
2022-03-29 16:01:00 -0600

    use the new context provider

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-import StateProvider from './context/store/fibonacci';
+import { AppContextProvider as StateProvider } from './context/store/index';
 
 ReactDOM.render(
   <React.StrictMode>
