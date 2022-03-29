83de098d3f0ad271582c02db5d8c9afff6c25503
Diego Ramirez <gustavormzrmz@gmail.com>
2022-03-29 16:00:03 -0600

    use the new context provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import './App.css';
 
 import ContainerRoot from './components/container/root';
@@ -6,7 +6,11 @@ import Header from './components/header';
 import ContainerBase from './components/container/base';
 import Fibonacci from './components/fibonacci';
 
+import { Store as ThemeStore } from './context/store/theme';
+
 const App = () => {
+  const { state } = useContext(ThemeStore);
+
   return (
     <ContainerRoot>
       <Header />
@@ -14,7 +18,7 @@ const App = () => {
         <div className='top-left-container'>
           <Fibonacci />
         </div>
-        <div className='bottom-right-container'>
+        <div className={ `bottom-right-container ${state.isDark && 'dark-primary'}` }>
           <h1 className='title'>Calculate Fibonacci!</h1>
         </div>
       </ContainerBase>
